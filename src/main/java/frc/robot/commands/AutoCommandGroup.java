06d6b25f16fb92fb684db5834bafc7d333971b9b
Cheru Berhanu <cheru@cheru.dev>
2023-03-28 10:03:37 -0400

    IT WORKSSSSSS!!!

--- src/main/java/frc/robot/commands/AutoCommandGroup.java
+++ src/main/java/frc/robot/commands/AutoCommandGroup.java
@@ -10,6 +10,10 @@ public class AutoCommandGroup extends SequentialCommandGroup {
     public AutoCommandGroup(IMUSubsystem imu, TankDriveSubsystem tank, IntakeSubsystem intake, double startpoint) {
         // TODO: Add your sequential commands in the super() call, e.g.
         //           super(new OpenClawCommand(), new MoveArmCommand());
-        super(new AutoScoreCommand(intake, tank), new AutoMoveToChargeCommand(imu, tank), new BalanceCommand(imu, tank, startpoint));
+        super(new AutoScoreCommand(intake, tank),
+                new AutoMoveToChargeCommand(imu, tank),
+                new AutoMovePastChargeCommand(imu, tank, startpoint),
+                new AutoMoveBackToChargeCommand(imu, tank),
+                new BalanceCommand(imu, tank, startpoint));
     }
 }
---
+++ src/main/java/frc/robot/commands/AutoMoveBackToChargeCommand.java
@@ -0,0 +1,43 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IMUSubsystem;
+import frc.robot.subsystems.TankDriveSubsystem;
+import static frc.robot.Constants.AutoConstants.ANGLESTOP;
+
+
+public class AutoMoveBackToChargeCommand extends CommandBase {
+    private final IMUSubsystem iMUSubsystem;
+    private final TankDriveSubsystem tankDriveSubsystem;
+    private double initialAngle;
+
+    public AutoMoveBackToChargeCommand(IMUSubsystem iMUSubsystem, TankDriveSubsystem tankDriveSubsystem) {
+        this.iMUSubsystem = iMUSubsystem;
+        this.tankDriveSubsystem = tankDriveSubsystem;
+        // each subsystem used by the command must be passed into the
+        // addRequirements() method (which takes a vararg of Subsystem)
+        addRequirements(this.iMUSubsystem, this.tankDriveSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        initialAngle = iMUSubsystem.getRoll();
+    }
+
+    @Override
+    public void execute() {
+        tankDriveSubsystem.setSpeed(-0.5, -0.5);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return iMUSubsystem.getRoll() > initialAngle + ANGLESTOP;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        tankDriveSubsystem.setSpeed(0.0, 0.0);
+        SmartDashboard.putBoolean("AutoMoveToCharge", false);
+    }
+}
